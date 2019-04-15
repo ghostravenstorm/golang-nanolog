81108b6939f33ce1461fd3a54f879b053f1afca1
Stuart <ghost.ravenstorm@gmail.com>
2019-04-15 12:20:16 -0400

    Added basic logging levels

--- nanolog.go
+++ nanolog.go
@@ -3,42 +3,122 @@ package nanolog
 
 import (
    "log"
-   // "fmt"
 )
 
-var DebugLevel int = 2
+var DebugLevel int = 3
 
 ///
 /// 0 = No Logging
 /// 1 = Errors only.
 /// 2 = Warnings and Errors.
-/// 3 = All loging.
+/// 3 = All info statments.
+/// 4 = Debugging.
 ///
 
 const (
-   NoLogging = iota
-   Error
-   Warning
-   Info
+   LnoLogging = iota
+   Lerror
+   Lwarning
+   Linfo
+   Ldebug
 )
 
 func init () () {
    log.SetFlags(log.Lshortfile)
 }
 
-func LogError (v ...interface{}) () {
-   if !(DebugLevel >= Error) { return }
-   log.Print(v)
+func Error (v ...interface{}) () {
+   if !(DebugLevel >= Lerror) { return }
+	b := []interface{}{"[ERR] "}
+	b = append(b, v...)
+   log.Print(b...)
 }
 
-func LogWarning () () {
+func Errorln (v ...interface{}) () {
+   if !(DebugLevel >= Lerror) { return }
+	b := []interface{}{"[ERR]"}
+	b = append(b, v...)
+   log.Println(b...)
+}
 
+func Errorf (format string, v ...interface{}) () {
+   if !(DebugLevel >= Lerror) { return }
+	b := []interface{}{"[ERR]"}
+	b = append(b, v...)
+   log.Printf(format, b...)
+}
+
+func Warning (v ...interface{}) () {
+   if !(DebugLevel >= Lwarning) { return }
+	b := []interface{}{"[WARN] "}
+	b = append(b, v...)
+   log.Print(b...)
+}
+
+func Warningln (v ...interface{}) () {
+	if !(DebugLevel >= Lwarning) { return }
+	b := []interface{}{"[WARN]"}
+	b = append(b, v...)
+   log.Println(b...)
+}
+
+func Warningf (format string, v ...interface{}) () {
+	if !(DebugLevel >= Lwarning) { return }
+	b := []interface{}{"[WARN]"}
+	b = append(b, v...)
+   log.Printf(format, b...)
+}
+
+func Info (v ...interface{}) () {
+   if !(DebugLevel >= Linfo) { return }
+	b := []interface{}{"[INFO] "}
+	b = append(b, v...)
+   log.Print(b...)
+}
+
+func Infoln (v ...interface{}) () {
+   if !(DebugLevel >= Linfo) { return }
+	b := []interface{}{"[INFO]"}
+	b = append(b, v...)
+   log.Println(b...)
+}
+
+func Infof (format string, v ...interface{}) () {
+   if !(DebugLevel >= Linfo) { return }
+	b := []interface{}{"[INFO]"}
+	b = append(b, v...)
+   log.Printf(format, b...)
+}
+
+func Debug (v ...interface{}) () {
+   if !(DebugLevel >= Ldebug) { return }
+	b := []interface{}{"[DEBUG] "}
+	b = append(b, v...)
+   log.Print(b...)
+}
+
+func Debugln (v ...interface{}) () {
+   if !(DebugLevel >= Ldebug) { return }
+	b := []interface{}{"[DEBUG]"}
+	b = append(b, v...)
+   log.Println(b...)
+}
+
+func Debugf (format string, v ...interface{}) () {
+   if !(DebugLevel >= Ldebug) { return }
+	b := []interface{}{"[DEBUG]"}
+	b = append(b, v...)
+   log.Printf(format, b...)
 }
 
 func Log (v ...interface{}) () {
-   log.Print(v)
+   log.Print(v...)
 }
 
-func Logln () () {
+func Logln (v ...interface{}) () {
+   log.Println(v...)
+}
 
+func Logf (format string, v ...interface{}) () {
+   log.Printf(format, v...)
 }
